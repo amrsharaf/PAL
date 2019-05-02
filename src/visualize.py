87e2c4e0771fc42bd26aa55b3e225d7704a4e454
amrsharaf <amr.n.sharaf@gmail.com>
2019-05-02 17:00:39 -0400

    skeleton for the figure

--- src/visualize.py
+++ src/visualize.py
@@ -1,2 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot():
+    x = np.arange(10)
+    y = np.arange(10)
+    plt.plot(x, y)
+    plt.show()
+    print('done plotting')
+
+
+def main():
+    plot()
+
+
 if __name__ == '__main__':
-    print('hello world!')
+    main()
