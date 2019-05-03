2b75aa9990648c8375386f33fa5236fdafde1cbf
amrsharaf <amr.n.sharaf@gmail.com>
2019-05-03 15:24:51 -0400

    remove old preprocess script and print better statements when splitting embeddings

--- src/preprocess.sh
+++
@@ -1,3 +0,0 @@
-python preprocess.py -i data/conll2003/eng.train -o data/conll2003/en.train;
-python preprocess.py -i data/conll2003/eng.testa -o data/conll2003/en.testa;
-python preprocess.py -i data/conll2003/eng.testb -o data/conll2003/en.testb;
--- src/split_embedddings.py
+++ src/split_embedddings.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 
+
 def is_english_line(line):
     return line.startswith('en:')
 
@@ -36,12 +37,13 @@ def parse_arguments():
     parser.add_argument('--output_path', required=True, type=str, help='Output path')
     return parser.parse_args()
 
-# TODO create command line flags
+
 def main():
     args = parse_arguments()
     language =  args.language
     input_file_path = args.input_path
     output_file_path = args.output_path
+    print('Reading embeddings from: ', input_file_path)
     with open(input_file_path, 'r', encoding='utf-8') as file_reader:
         print('opened file!')
         # Read all input lines
@@ -52,7 +54,7 @@ def main():
         # Write the language lines to an output file
         with open(output_file_path, 'w', encoding='utf-8') as output_writer:
             output_writer.writelines(english_lines)
-        print('done writing file!')
+        print('done writing file to: ', output_file_path)
 
 
 if __name__ == '__main__':
